0a1f27a481e52c6467d42c188a4188bdafb204d1
Michelle Lee <michellelee@Copenhagen.local>
2022-02-02 13:54:05 +0900

    Updates

---
+++ component---src-pages-notion-js-5fad97b3e743356a9ab5.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgatsby_starter_portfolio_minimal_theme=self.webpackChunkgatsby_starter_portfolio_minimal_theme||[]).push([[782],{9162:function(e,t,n){n.r(t),n.d(t,{default:function(){return r}});var l=n(7294),a=n(680);function r(){return l.createElement(l.Fragment,null,l.createElement(a.pQ,{title:"Notion articles",useTitleTemplate:!0,noIndex:!0}),l.createElement(a.T3,null,l.createElement(a.ez,{sectionId:"about",heading:"About Michelle"})))}}}]);
+//# sourceMappingURL=component---src-pages-notion-js-5fad97b3e743356a9ab5.js.map
